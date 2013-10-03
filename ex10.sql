94179d75d6a74e42da36b6886341ac6ee0992d52
Chris Lansing <cslansin@gmail.com>
2013-10-03 11:18:39 -0400

    ex 5-10; still easy stuff

---
+++ ex10.sql
@@ -0,0 +1,10 @@
+select * from pet;
+
+update pet set name = "Zed's Pet" where id in (
+    select pet.id
+    from pet, person_pet, person_id and
+    pet.id = person_pet.pet_id and
+    person.first_name = "Zed"
+);
+
+select * from pet;
--- ex5.sql
+++ ex5.sql
@@ -2,3 +2,7 @@ select * from person;
 select name, age from pet;
 select name, age from pet where dead = 0;
 select * from person where first_name != "Zed";
+
+select * from pet where age > 10;
+select * from person where age < (select age from person where name = "Chris");
+select * from person where first_name != "Zed" and age < 30;
